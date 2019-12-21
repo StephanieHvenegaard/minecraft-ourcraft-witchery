e12ae1cafb1bd832e569ed6657edbe0fdf9d21f0
Stephanie Night <s.hvenegaard@outlook.com>
2019-12-21 13:59:12 +0100

    fixed dependencies, and naming

--- src/main/java/com/thenights/ourcraft/witchery/core/OurcraftWithery.java
+++ src/main/java/com/thenights/ourcraft/witchery/core/OurcraftWithery.java
@@ -1,7 +1,9 @@
-package com.example.examplemod;
+package com.thenights.ourcraft.witchery.core;
 
+import com.the_nights.ourcraft.core.tap.OurcraftCoreTap;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,13 +21,16 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(OurcraftWithery.MODID)
+public class OurcraftWithery
 {
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static OurcraftWithery INSTANCE;                                             // public instance of the main mod class
+    public static final String MODID = "ourcraft-witchery";                             // Mod id
+    public static final ItemGroup OURCRAFT_GROUP = new OurcraftCoreTap();               // creative tap
+    public static final Logger LOGGER = LogManager.getLogger(MODID);                    // Logger
 
-    public ExampleMod() {
+    public OurcraftWithery() {
+        INSTANCE = this;
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -54,7 +59,7 @@ public class ExampleMod
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        // InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
